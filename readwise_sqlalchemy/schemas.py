cf83bb9d00d72e72d6b42c79a89be6d02b374224
chrisbillows <christopherbillows@gmail.com>
2025-03-07 21:54:35 +0000

    #17: WIP commits

--- readwise_sqlalchemy/schemas.py
+++ readwise_sqlalchemy/schemas.py
@@ -2,19 +2,21 @@ import json
 from datetime import datetime
 from typing import Optional
 
-from pydantic import AnyUrl, BaseModel, Field, HttpUrl
+from pydantic import AnyUrl, BaseModel, Field, field_validator, HttpUrl
 
 
-class HighlightTagsSchema(BaseModel):
+# `extra=forbid` causes extra fields to raise an error. They are accepted by default. 
+class HighlightTagsSchema(BaseModel, extra='forbid'):
     """
     Validate 'tags' fields in a HighlightSchema highlight.
     """
+    # Undocumented. Seems likely not null in practice but won't enforce. Default to 
+    # None in case they aren't both required?
+    id: Optional[int] = None
+    name: Optional[str] = None
 
-    id: int
-    name: str
 
-
-class HighlightSchema(BaseModel):
+class HighlightSchema(BaseModel, extra='forbid'):
     """
     Validate 'highlights' fields output by the Readwise 'Highlight EXPORT' endpoint.
 
@@ -46,15 +48,34 @@ class HighlightSchema(BaseModel):
     end_location: None  # Only example in docs. What is allowed?
     url: Optional[AnyUrl] = Field(max_length=4095)
     book_id: int = Field(gt=0, strict=True)  # See 'user_book_id'.
-    tags: Optional[
-        list[HighlightTagsSchema]
-    ]  # Pydantic accepts empty lists by default.
+    # Undocumented. Assume tags will be strings. Nulls possible, not seen in 
+    # user data and handled @field_validator. Pydantic accepts empty lists by default.
+    tags: Optional[list[HighlightTagsSchema]]
     is_favorite: Optional[bool] = Field(strict=True)
     is_discard: Optional[bool] = Field(strict=True)
     readwise_url: Optional[HttpUrl]
+    
+    @field_validator("tags", mode='before')
+    @classmethod
+    def replace_null_with_empty_list(cls: BaseModel, value: Optional[list]) -> list:
+        """
+        Replace a null value with an empty list.
+        
+        Parameters
+        ----------
+        cls: BaseModel
+            A Pydantic Schema that inherits from ``Pydantic.BaseModel``
+        
+        Returns
+        -------
+        list
+            The passed value if it's a list, or an empty list.
+        
+        """
+        return value if value else []
 
 
-class BookSchema(BaseModel):
+class BookSchema(BaseModel, extra='forbid'):
     """
     Validate books output by the Readwise 'Highlight EXPORT' endpoint.
 
@@ -74,14 +95,27 @@ class BookSchema(BaseModel):
     cover_image_url: Optional[HttpUrl] = Field(max_length=2047)  # Used 'image_url'.
     unique_url: Optional[HttpUrl]
     summary: Optional[str]
-    book_tags: list[Optional[str]]  # Undocumented. Assume tags will be strings.
+    # Undocumented. Assume book_tags will be strings. Nulls possible, not seen in 
+    # user data and handled @field_validator.
+    book_tags: list[str]
     category: str = Field(pattern="^(books|articles|tweets|podcasts)$")
+    # Undocumented but user data is always null. Docs use "" in examples.
+    document_note: Optional[str]
     readwise_url: HttpUrl
     source_url: Optional[AnyUrl] = Field(max_length=2047)
     asin: Optional[str] = Field(
         min_length=10, max_length=10, pattern="^[A-Z0-9]{10}$"
     )  # Used Amazon Standard Identification Number.
     highlights: list[HighlightSchema]
+    
+
+    @field_validator("book_tags", mode='before')
+    @classmethod
+    def replace_null_with_empty_list(cls: BaseModel, value: Optional[list]) -> list:
+        """
+        See duplicate method on BookSchema.
+        """
+        return value if value else []
 
 
 if __name__ == "__main__":
@@ -89,10 +123,24 @@ if __name__ == "__main__":
     with open("tests/data/real/sample_all_24th_nov_1604.json", "r") as file_handle:
         data = json.load(file_handle)
     count = 0
+    highlight_ids = []
+    reused_highlight_ids = []
     for book in data:
         try:
             pydantic_book = BookSchema(**book)
             count += 1
+            for highlight in book['highlights']:
+                for highlight_tag in highlight['tags']:
+                    if highlight_tag['name'] == "orange":
+                        print(highlight_tag)
+                    # id = highlight_tag['id']
+                    # if id not in highlight_ids:
+                    #     highlight_ids.append(id)
+                    # else:
+                    #     reused_highlight_ids.append(id)
+            
         except ValueError as err:
             print(f"🚨 Validation error {err} for {book['title']}")
+    print(len(highlight_ids))
+    print(reused_highlight_ids)
     print(f"Validated {count} books")
--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,9 @@ mock_book: dict
 mock_highlight: dict
     Mock a valid highlight dictionary returned by the Readwise 'Highlight EXPORT'
     endpoint as part of a book dictionary.
+    
+mock_highlight_tag: dict
+
 
 mock_user_config: UserConfig
     Mock a valid readwise_sqlalchemy user configuration with a 'tmp_path' as user dir.
@@ -42,6 +45,13 @@ def mock_highlight() -> dict:
     """Valid Readwise mock highlight."""
     fields = HIGHLIGHT_SCHEMA_VARIANTS.keys()
     return {field: HIGHLIGHT_SCHEMA_VARIANTS[field]["value_valid"] for field in fields}
+
+
+@pytest.fixture
+def mock_highlight_tags() -> dict:
+    """Valid Readwise mock highlight tags."""
+    fields = HIGHLIGHT_TAGS_SCHEMA_VARIANTS.keys()
+    return {field: HIGHLIGHT_TAGS_SCHEMA_VARIANTS[field]["value_valid"] for field in fields}
 
 
 @pytest.fixture
@@ -87,51 +97,56 @@ BOOK_SCHEMA_VARIANTS = {
         "value_invalid_type": "a",
         "nullable": True,
     },
-    # "cover_image_url": {
-    #     "value_valid": "http://www.image.com/image.jpg",
-    #     "value_invalid_type": "not_a_web_address",
-    #     "nullable": True,
-    # },
-    # "unique_url": {
-    #     "value_valid": "http://www.article.com/article",
-    #     "value_invalid_type": (),
-    #     "nullable": True,
-    # },
-    # "summary": {
-    #     "value_valid": "An example summary",
-    #     "value_invalid_type": 987,
-    #     "nullable": True,
-    # },
+    "cover_image_url": {
+        "value_valid": "http://www.image.com/image.jpg",
+        "value_invalid_type": "not_a_web_address",
+        "nullable": True,
+    },
+    "unique_url": {
+        "value_valid": "http://www.article.com/article",
+        "value_invalid_type": (),
+        "nullable": True,
+    },
+    "summary": {
+        "value_valid": "An example summary",
+        "value_invalid_type": 987,
+        "nullable": True,
+    },
     "book_tags": {
         "value_valid": ["yellow", "green"],
         "value_invalid_type": [1, 2, 3],
+        "nullable": True,  # Field validator converts to []
+    },
+    "category": {
+        "value_valid": "books",
+        "value_invalid_type": "youtube",
         "nullable": False,
     },
-    # "category": {
-    #     "value_valid": "books",
-    #     "value_invalid_type": "youtube",
-    #     "nullable": False,
-    # },
-    # "readwise_url": {
-    #     "value_valid": "http://www.readwise.io/book123",
-    #     "value_invalid_type": "a_normal_string",
-    #     "nullable": False,
-    # },
-    # "source_url": {
-    #     "value_valid": "http://www.source.com/the_source",
-    #     "value_invalid_type": "a_normal_string",
-    #     "nullable": True,
-    # },
-    # "asin": {
-    #     "value_valid": "A0099BC1Z0",
-    #     "value_invalid_type": "A00-BC-1Z0",
-    #     "nullable": True,
-    # },
-    # "highlights": {
-    #     "value_valid": [],
-    #     "value_invalid_type": 123,
-    #     "nullable": False,
-    # },
+        "document_note": {
+        "value_valid": "A string",
+        "value_invalid_type": 1000,
+        "nullable": True,
+    },
+    "readwise_url": {
+        "value_valid": "http://www.readwise.io/book123",
+        "value_invalid_type": "a_normal_string",
+        "nullable": False,
+    },
+    "source_url": {
+        "value_valid": "http://www.source.com/the_source",
+        "value_invalid_type": "a_normal_string",
+        "nullable": True,
+    },
+    "asin": {
+        "value_valid": "A0099BC1Z0",
+        "value_invalid_type": "A00-BC-1Z0",
+        "nullable": True,
+    },
+    "highlights": {
+        "value_valid": [],
+        "value_invalid_type": 123,
+        "nullable": False,
+    },
 }
 
 
@@ -147,79 +162,93 @@ HIGHLIGHT_SCHEMA_VARIANTS = {
         "value_invalid_type": 987654321,
         "nullable": False,
     },
-    # "location": {
-    #     "value_valid": 12345,
-    #     "value_invalid_type": "one two three",
-    #     "nullable": True,
-    # },
-    # "location_type": {
-    #     "value_valid": "page",
-    #     "value_invalid_type": "time",
-    #     "nullable": True,
-    # },
-    # "note": {
-    #     "value_valid": "",
-    #     "value_invalid_type": [],
-    #     "nullable": True,
-    # },
-    # "color": {
-    #     "value_valid": "yellow",
-    #     "value_invalid_type": "magenta",
-    #     "nullable": True,
-    # },
-    # "highlighted_at": {
-    #     "value_valid": "2025-01-01T01:02:04.456Z",
-    #     "value_invalid_type": "2024-13-01T10:20:30.123Z",
-    #     "nullable": True,
-    # },
-    # "created_at": {
-    #     "value_valid": "2025-01-01T01:02:04.456Z",
-    #     "value_invalid_type": "2024-13-01T10:20:30.123Z",
-    #     "nullable": True,
-    # },
-    # "updated_at": {
-    #     "value_valid": "2025-01-01T01:02:04.456Z",
-    #     "value_invalid_type": "2024-13-01T10:20:30.123Z",
-    #     "nullable": True,
-    # },
-    # "external_id": {
-    #     "value_valid": "6320b2bd7fbcdd7b0c000b3e",
-    #     "value_invalid_type": 12345,
-    #     "nullable": True,
-    # },
-    # "end_location": {
-    #     "value_valid": None,
-    #     "value_invalid_type": 12345,
-    #     "nullable": True,
-    # },
-    # "url": {
-    #     "value_valid": "http://www.url.com",
-    #     "value_invalid_type": "not-a-url",
-    #     "nullable": True,
-    # },
+    "location": {
+        "value_valid": 12345,
+        "value_invalid_type": "one two three",
+        "nullable": True,
+    },
+    "location_type": {
+        "value_valid": "page",
+        "value_invalid_type": "time",
+        "nullable": True,
+    },
+    "note": {
+        "value_valid": "",
+        "value_invalid_type": [],
+        "nullable": True,
+    },
+    "color": {
+        "value_valid": "yellow",
+        "value_invalid_type": "magenta",
+        "nullable": True,
+    },
+    "highlighted_at": {
+        "value_valid": "2025-01-01T01:02:03.456Z",
+        "value_invalid_type": "2024-13-01T10:20:30.123Z",
+        "nullable": True,
+    },
+    "created_at": {
+        "value_valid": "2025-01-01T01:02:03.456Z",
+        "value_invalid_type": "2024-13-01T10:20:30.123Z",
+        "nullable": True,
+    },
+    "updated_at": {
+        "value_valid": "2025-01-01T01:02:03.456Z",
+        "value_invalid_type": "2024-13-01T10:20:30.123Z",
+        "nullable": True,
+    },
+    "external_id": {
+        "value_valid": "6320b2bd7fbcdd7b0c000b3e",
+        "value_invalid_type": 12345,
+        "nullable": True,
+    },
+    "end_location": {
+        "value_valid": None,
+        "value_invalid_type": 12345,
+        "nullable": True,
+    },
+    "url": {
+        "value_valid": "http://www.url.com",
+        "value_invalid_type": "not-a-url",
+        "nullable": True,
+    },
     "book_id": {
         "value_valid": 1,
         "value_invalid_type": -1,
         "nullable": False,
     },
-    # "tags": {  # The 'HighlightTagsSchema' is simple enough to test here.
-    #     "value_valid": [{"id": 1234, "name": "tag_2"}],
-    #     "value_invalid_type": [{"id": 1234, "name": 1234}],
-    #     "nullable": True,
-    # },
-    # "is_favorite": {
-    #     "value_valid": True,
-    #     "value_invalid_type": "true",
-    #     "nullable": True,
-    # },
-    # "is_discard": {
-    #     "value_valid": False,
-    #     "value_invalid_type": "false",
-    #     "nullable": True,
-    # },
-    # "readwise_url": {
-    #     "value_valid": "https://readwise.io/open/123456",
-    #     "value_invalid_type": "readwise.io/open/123456",
-    #     "nullable": True,
-    # },
+    "tags": {
+        "value_valid": [],
+        "value_invalid_type": [{'unexpected_field': 'value'}],
+        "nullable": True,
+    },
+    "is_favorite": {
+        "value_valid": True,
+        "value_invalid_type": "true",
+        "nullable": True,
+    },
+    "is_discard": {
+        "value_valid": False,
+        "value_invalid_type": "false",
+        "nullable": True,
+    },
+    "readwise_url": {
+        "value_valid": "https://readwise.io/open/123456",
+        "value_invalid_type": "readwise.io/open/123456",
+        "nullable": True,
+    },
+}
+
+# 'value_invalid_type' is an example of one possible invalid type. It is not exhaustive.
+HIGHLIGHT_TAGS_SCHEMA_VARIANTS = {
+    "id": {
+        "value_valid": 123456,
+        "value_invalid_type": "one",
+        "nullable": True,
+    },
+    "name": {
+        "value_valid": "label",
+        "value_invalid_type": 1,
+        "nullable": True,
+    }
 }

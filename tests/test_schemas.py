cab5cc08158557ab1f53bb532f44272ea82fb91f
chrisbillows <christopherbillows@gmail.com>
2025-03-07 21:53:23 +0000

    #17: Rename `pydantic.py` as `schemas.py` and tests

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -3,14 +3,82 @@ from typing import Any
 import pytest
 from pydantic import ValidationError
 
-from readwise_sqlalchemy.pydantic import BookSchema, HighlightSchema
+from readwise_sqlalchemy.sql_alchemy import Book
+from readwise_sqlalchemy.pydantic import BookSchema, HighlightSchema, HighlightTagsSchema
 
-from .conftest import BOOK_SCHEMA_VARIANTS, HIGHLIGHT_SCHEMA_VARIANTS
+from .conftest import (
+    BOOK_SCHEMA_VARIANTS, HIGHLIGHT_SCHEMA_VARIANTS, HIGHLIGHT_TAGS_SCHEMA_VARIANTS
+)
+
+
+def test_highlight_tags_schema_with_valid_values(mock_highlight_tags: dict):
+    assert HighlightTagsSchema(**mock_highlight_tags)
+
+
+@pytest.mark.parametrize("invalid_field", HIGHLIGHT_TAGS_SCHEMA_VARIANTS.keys())
+def test_highlight_tags_schema_with_invalid_types(
+    invalid_field: str, mock_highlight_tags: dict
+): 
+    mock_highlight_tags[invalid_field] = HIGHLIGHT_TAGS_SCHEMA_VARIANTS[invalid_field][
+        "value_invalid_type"
+    ]
+    with pytest.raises(ValidationError):
+        HighlightSchema(**mock_highlight_tags)    
+
+
+@pytest.mark.parametrize(
+    "valid_null_field",
+    [
+        field
+        for field in HIGHLIGHT_TAGS_SCHEMA_VARIANTS.keys()
+        if HIGHLIGHT_TAGS_SCHEMA_VARIANTS[field]["nullable"]
+    ],
+)
+def test_highlight_tags_schema_with_null_values_where_allowed(
+    valid_null_field: str, mock_highlight_tags: dict
+):
+    mock_highlight_tags[valid_null_field] = None
+    assert HighlightTagsSchema(**mock_highlight_tags)
+
+
+# Skips as currently no invalid null fields.
+@pytest.mark.parametrize(
+    "invalid_null_field",
+    [
+        field
+        for field in HIGHLIGHT_TAGS_SCHEMA_VARIANTS.keys()
+        if not HIGHLIGHT_TAGS_SCHEMA_VARIANTS[field]["nullable"]
+    ],
+)
+def test_highlight_tags_schema_with_null_values_where_not_allowed(
+    invalid_null_field: str, mock_highlight_tags: dict
+):
+    mock_highlight_tags[invalid_null_field] = None
+    with pytest.raises(ValidationError):
+        HighlightTagsSchema(**mock_highlight_tags)
+
+
+# Raise if any field is missing.
+@pytest.mark.parametrize(
+    "removed_field", [field for field in HIGHLIGHT_TAGS_SCHEMA_VARIANTS.keys()]
+)
+def test_highlight_tags_schema_with_missing_fields(
+    removed_field: str, mock_highlight_tags: dict[str, Any]
+):
+    del mock_highlight_tags[removed_field]
+    # Missing fields are intended to be allowed.
+    HighlightTagsSchema(**mock_highlight_tags)
+
+
+def test_highlight_tags_schema_config_with_unexpected_field(mock_highlight_tags: dict):
+    mock_highlight_tags['extra_field'] = None
+    with pytest.raises(ValidationError):
+        HighlightTagsSchema(**mock_highlight_tags)
 
 
 def test_highlight_schema_with_valid_values(mock_highlight: dict):
     assert HighlightSchema(**mock_highlight)
-
+       
 
 @pytest.mark.parametrize("invalid_field", HIGHLIGHT_SCHEMA_VARIANTS.keys())
 def test_highlight_schema_with_invalid_types(
@@ -19,6 +87,8 @@ def test_highlight_schema_with_invalid_types(
     mock_highlight[invalid_field] = HIGHLIGHT_SCHEMA_VARIANTS[invalid_field][
         "value_invalid_type"
     ]
+    if invalid_field == 'tags':
+        breakpoint()
     with pytest.raises(ValidationError):
         HighlightSchema(**mock_highlight)
 
@@ -64,6 +134,18 @@ def test_highlight_schema_with_missing_fields(
     del mock_highlight[removed_field]
     with pytest.raises(ValidationError):
         HighlightSchema(**mock_highlight)
+
+
+def test_highlight_schema_config_with_unexpected_field(mock_highlight:dict):
+    mock_highlight['extra_field'] = None
+    with pytest.raises(ValidationError):
+        HighlightSchema(**mock_highlight)
+
+
+def test_highlight_replace_null_with_empty_list_for_tags(mock_highlight: dict):
+    mock_highlight['tags'] = None
+    highlight = HighlightSchema(**mock_highlight)
+    assert highlight.tags == []
 
 
 def test_book_schema_with_valid_values(mock_book: dict):
@@ -116,3 +198,18 @@ def test_book_schema_with_missing_fields(removed_field: str, mock_book: dict[str
     del mock_book[removed_field]
     with pytest.raises(ValidationError):
         BookSchema(**mock_book)
+
+        
+def test_book_schema_config_with_unexpected_field(mock_book: dict):
+    mock_book['extra_field'] = None
+    with pytest.raises(ValidationError):
+        HighlightSchema(**mock_book)
+
+
+def test_book_replace_null_with_empty_list_for_book_tags(mock_book: dict):
+    mock_book['book_tags'] = None
+    book = BookSchema(**mock_book)
+    assert book.book_tags == []
+
+
+

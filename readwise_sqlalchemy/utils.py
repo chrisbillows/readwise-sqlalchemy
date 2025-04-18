da379b279d6baf05652da0d9dbc0404ae40d7119
chrisbillows <christopherbillows@gmail.com>
2025-04-18 15:30:17 +0100

    #35: Add func to create e2e test data file

--- readwise_sqlalchemy/utils.py
+++ readwise_sqlalchemy/utils.py
@@ -6,6 +6,9 @@ from typing import Any
 from sqlalchemy.inspection import inspect
 from sqlalchemy.orm import DeclarativeBase
 
+from readwise_sqlalchemy.config import USER_CONFIG, UserConfig
+from readwise_sqlalchemy.main import fetch_from_export_api
+
 logger = logging.getLogger(__name__)
 
 
@@ -13,11 +16,13 @@ class FileHandler:
     """Handle file I/O."""
 
     @staticmethod
-    def write_json(data: dict[Any, Any], file_path: Path) -> None:
+    def write_json(
+        data: dict[Any, Any] | list[dict[Any, Any]], file_path: Path
+    ) -> None:
         """Static method to write json."""
         with open(file_path, "w") as file_handle:
             json.dump(data, file_handle)
-        logging.info(f"Written to: {file_path}")
+        logger.info(f"Written to: '{file_path}'")
 
     @staticmethod
     def read_json(file_path: Path | str) -> Any:
@@ -40,3 +45,50 @@ def get_columns_and_values(orm_mapped_obj: DeclarativeBase) -> dict[str, Any]:
         column.key: getattr(orm_mapped_obj, column.key)
         for column in inspect(orm_mapped_obj).mapper.column_attrs
     }
+
+
+def create_real_user_data_json_for_end_to_end_testing(
+    user_config: UserConfig = USER_CONFIG,
+) -> None:
+    """
+    Fetch your real Readwise highlights and store locally as test data.
+
+    FOR DEVELOPERS: This function fetches all your Readwise books and highlights and
+    saves it as JSON for end-to-end testing.
+
+    *THE FUNCTION MUST BE INVOKED BY YOU. YOUR DATA NEVER LEAVES YOUR MACHINE*
+
+    Your data is saved locally as `~/readwise-sqlalchemy/my_readwise_highlights.json`.
+    When pytest is invoked, if `my_readwise_highlights.json` exists, an end-to-end test
+    is run. See the test at `tests/e2e/test_main.py`.
+
+    To create `my_readwise_highlights.json` so the e2e tests runs, navigate to
+    the project directory in your CLI and run:
+
+    ```
+    python3 -m readwise_sqlalchemy.utils
+    ```
+
+    This runs the `if __name__ == "__main__"` block at the bottom of this file.
+
+    Parameters
+    ----------
+    user_config: UserConfig, default = USER_CONFIG
+        A UserConfig object.
+    """
+    target_file_path = user_config.app_dir / "my_readwise_highlights.json"
+
+    logger.info(
+        "Hello Developer! Fetching your Readwise highlights and writing to JSON."
+    )
+    api_content = fetch_from_export_api(last_fetch=None, user_config=user_config)
+    FileHandler.write_json(api_content, target_file_path)
+
+    logger.info(
+        "When you next run pytest, the e2e test should run automatically. P.S.YOUR "
+        "DATA NEVER LEAVES YOUR MACHINE."
+    )
+
+
+if __name__ == "__main__":
+    create_real_user_data_json_for_end_to_end_testing()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,13 @@
+import json
+from unittest.mock import MagicMock, patch
+
 import pytest
 
-from readwise_sqlalchemy.utils import get_columns_and_values
+from readwise_sqlalchemy.config import UserConfig
+from readwise_sqlalchemy.utils import (
+    create_real_user_data_json_for_end_to_end_testing,
+    get_columns_and_values,
+)
 
 
 @pytest.mark.skip("To complete implementation")
@@ -24,3 +31,18 @@ def test_get_columns_and_values(minimal_book_as_orm):
         "batch_id": 1,
     }
     assert actual == expected
+
+
+@patch("readwise_sqlalchemy.utils.fetch_from_export_api")
+def test_create_real_user_data_json_for_end_to_end_testing(
+    mock_fetch_from_export_api: MagicMock, mock_user_config: UserConfig
+):
+    mock_books = ["book_1", "book_2"]
+    mock_fetch_from_export_api.return_value = mock_books
+
+    actual = create_real_user_data_json_for_end_to_end_testing(mock_user_config)
+
+    with open(mock_user_config.app_dir / "my_readwise_highlights.json") as file_handle:
+        actual_content = json.load(file_handle)
+        assert actual_content == mock_books
+        assert actual is None
